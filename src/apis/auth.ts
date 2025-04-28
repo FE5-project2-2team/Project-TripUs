044e17c255827a4f6c9747d9527bb55c638d2244
run3go <qkrwjdtn09@gmail.com>
2025-04-28 22:44:34 +0900

    feat: API_회원가입 및 로그인, 로그아웃 API 구현

---
+++ src/apis/auth.ts
@@ -0,0 +1,77 @@
+import { AxiosError } from "axios";
+import { axiosInstance } from "./axios";
+
+interface UserInfo {
+  email: string;
+  password: string;
+  fullName: {
+    name: string;
+    Nickname: string;
+    age: number;
+    gender: string;
+  };
+}
+
+export const registerUser = async (userInfo: UserInfo) => {
+  const { email, fullName, password } = userInfo;
+  try {
+    const { data } = await axiosInstance.post("/signup", {
+      email,
+      fullName: JSON.stringify(fullName),
+      password,
+    });
+    return data.user;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      console.log(error.response?.data);
+    } else {
+      console.log("Unknwon Error", error);
+    }
+  }
+};
+
+export const loginUser = async (loginData: {
+  email: string;
+  password: string;
+}) => {
+  const { email, password } = loginData;
+  try {
+    const { data } = await axiosInstance.post("/login", {
+      email,
+      password,
+    });
+    return data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      console.log(error.message);
+    } else {
+      console.log("Unknwon Error", error);
+    }
+  }
+};
+
+export const logoutUser = async () => {
+  try {
+    const { data } = await axiosInstance.post("/logout");
+    console.log(data);
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      console.log(error.message);
+    } else {
+      console.log("Unknwon Error", error);
+    }
+  }
+};
+
+export const fetchAuthUser = async () => {
+  try {
+    const { data } = await axiosInstance.get("/auth-user");
+    return data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      console.log(error.message);
+    } else {
+      console.log("Unknwon Error", error);
+    }
+  }
+};
---
+++ src/apis/axios.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { APIUrl } from "../constants/apis";
+import { useAuthStore } from "../store/authStore";
+
+export const axiosInstance = axios.create({
+  baseURL: APIUrl,
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = useAuthStore.getState().accessToken;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
