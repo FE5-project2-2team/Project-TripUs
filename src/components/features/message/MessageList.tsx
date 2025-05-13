bd702bc684548b88cd2bd91b3de485a92a24746f
KwonTaeHun00 <rnjsxogns0@naver.com>
2025-05-14 01:47:38 +0900

    [feat] Message_읽음 처리, 프로필 이미지 및 시간 표시, 데이터 갱신 추가

--- src/components/features/message/MessageList.tsx
+++ src/components/features/message/MessageList.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import ChattingComponent from "./ChattingComponent";
 
 interface MessageListProps {
@@ -6,17 +7,43 @@ interface MessageListProps {
 }
 
 export default function MessageList({ messages, myUserId }: MessageListProps) {
+	const bottomRef = useRef<HTMLDivElement | null>(null);
+
+	useEffect(() => {
+		bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+	}, [messages]);
 	return (
 		<div className="flex flex-col px-4 py-2 overflow-y-auto h-full">
-			{messages.map((msg) => (
-				<ChattingComponent
-					key={msg._id}
-					message={msg.message}
-					sender={msg.sender}
-					createdAt={msg.createdAt}
-					isMe={msg.sender._id === myUserId}
-				/>
-			))}
+			{messages.map((msg, idx) => {
+				const isMe = msg.sender._id === myUserId;
+
+				const previous = messages[idx - 1];
+				const isFirstOfGroup =
+					!previous || previous.sender._id !== msg.sender._id;
+
+				let showTime = true;
+				if (previous) {
+					const currentTime = new Date(msg.createdAt).getTime();
+					const prevTime = new Date(previous.createdAt).getTime();
+					const diff = currentTime - prevTime;
+					if (diff < 60 * 1000) showTime = false;
+				}
+
+				return (
+					<ChattingComponent
+						key={msg._id}
+						message={msg.message}
+						sender={msg.sender}
+						createdAt={msg.createdAt}
+						isMe={isMe}
+						seen={msg.seen}
+						showProfileImage={!isMe && isFirstOfGroup}
+						showTime={showTime}
+						isFirstOfGroup={isFirstOfGroup}
+					/>
+				);
+			})}
+			<div ref={bottomRef} />
 		</div>
 	);
 }
