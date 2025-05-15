9763e9ed30cf5369894d69258726f569b25109fa
run3go <qkrwjdtn09@gmail.com>
2025-05-15 22:19:03 +0900

    [fix] PostCreate_게시글 수정 시 인원 수치 변경

--- src/components/commons/CustomSelect.tsx
+++ src/components/commons/CustomSelect.tsx
@@ -1,7 +1,8 @@
 import { useRef, useState } from "react";
-import { useFormContext } from "react-hook-form";
+import { useController, useFormContext } from "react-hook-form";
 import { useClickAway } from "react-use";
 import { twMerge } from "tailwind-merge";
+import { usePostStore } from "../../store/postStore";
 
 export default function CustomSelect({
 	isEditing,
@@ -14,7 +15,12 @@ export default function CustomSelect({
 	label: string;
 	options: { label: string | number; value: string | number }[];
 }) {
-	const { register, setValue, watch } = useFormContext();
+	const postInfo = usePostStore((state) => state.postInfo);
+	const { control } = useFormContext();
+	const {
+		field: { value, onChange }
+	} = useController({ name, control });
+
 	const [optionOepn, setOptionOpen] = useState(false);
 	const optionsRef = useRef<HTMLUListElement | null>(null);
 
@@ -26,21 +32,26 @@ export default function CustomSelect({
 		setOptionOpen(false);
 	});
 
+	if (!postInfo) return;
+
 	return (
 		<div className="relative">
 			<label
 				htmlFor={name}
-				className={twMerge("post-input-title", isEditing && "text-[#aaaaaa]")}
+				className={twMerge(
+					"post-input-title",
+					name === "channel" && isEditing && "text-[#aaaaaa]"
+				)}
 			>
 				{label}
 			</label>
 			<input
 				onClick={toggleOptionHandler}
-				className="input-style focus:outline-0 cursor-pointer"
+				className="input-style focus:outline-0 cursor-pointer disabled:text-[#aaaaaa]"
 				type="text"
 				readOnly
-				{...register(name)}
-				value={options.find((opt) => opt.value === watch(name))?.label || ""}
+				value={options.find((opt) => opt.value === value)?.label || ""}
+				disabled={name === "channel" && isEditing}
 			/>
 			{optionOepn && (
 				<ul
@@ -50,18 +61,23 @@ export default function CustomSelect({
 						"dark:bg-[#333] dark:border-[#000] z-20"
 					)}
 				>
-					{options.map((option) => (
-						<li
-							onClick={() => {
-								toggleOptionHandler();
-								setValue(name, option.value);
-							}}
-							key={option.value}
-							className="px-5 py-2 rounded-sm hover:bg-[#E0F4F2] hover:text-[#06b796] cursor-pointer dark:hover:text-[#333]"
-						>
-							{option.label}
-						</li>
-					))}
+					{options
+						.filter((option) => {
+							if (!isEditing) return true;
+							return postInfo?.memberLimit <= (option.label as number);
+						})
+						.map((option) => (
+							<li
+								onClick={() => {
+									toggleOptionHandler();
+									onChange(option.value);
+								}}
+								key={option.value}
+								className="px-5 py-2 rounded-sm hover:bg-[#E0F4F2] hover:text-[#06b796] cursor-pointer dark:hover:text-[#333]"
+							>
+								{option.label}
+							</li>
+						))}
 				</ul>
 			)}
 		</div>
