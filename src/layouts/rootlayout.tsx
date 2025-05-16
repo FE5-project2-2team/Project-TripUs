0ae96d014595ed5c100386c5d61730d31ea8317f
run3go <qkrwjdtn09@gmail.com>
2025-05-16 21:06:50 +0900

    [style] 메시지 반응형 스타일링 1차

--- src/layouts/rootlayout.tsx
+++ src/layouts/rootlayout.tsx
@@ -4,7 +4,12 @@ import Header from "../components/commons/Header";
 export default function RootLayout() {
 	const location = useLocation();
 
-	const hideHeaderPaths = ["/post/detail", "/postCreate", "/post/edit"];
+	const hideHeaderPaths = [
+		"/post/detail",
+		"/postCreate",
+		"/post/edit",
+		"/message"
+	];
 
 	const hideHeader = hideHeaderPaths.some((path) =>
 		location.pathname.startsWith(path)
