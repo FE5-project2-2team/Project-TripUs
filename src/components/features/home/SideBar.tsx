942b542b086ea65ed4872be3f6cda5cdc6c0c2e9
run3go <qkrwjdtn09@gmail.com>
2025-05-17 15:35:22 +0900

    [fix] Header_헤더 스타일 수정

--- src/components/features/home/SideBar.tsx
+++ src/components/features/home/SideBar.tsx
@@ -1,27 +1,25 @@
-import { useState } from "react";
 import { useNavigate } from "react-router";
 import { twMerge } from "tailwind-merge";
 import logoCircle from "../../../assets/images/logo_circle.png";
 import { useAuthStore } from "../../../store/authStore";
 import { useThemeStore } from "../../../store/themeStore";
 import Icon from "../../commons/Icon";
-import UserListModal from "../user/UserListModal";
 
 export default function SideBar({
 	sidebarOpen,
 	signOut,
-	toggleHandler
+	toggleHandler,
+	toggleUserList
 }: {
 	sidebarOpen: boolean;
 	signOut: () => void;
 	toggleHandler: () => void;
+	toggleUserList: () => void;
 }) {
 	const navigate = useNavigate();
 	const userInfo = useAuthStore((state) => state.userInfo);
 	const userData = useAuthStore((state) => state.userData);
 	const { isDark, toggleTheme } = useThemeStore();
-
-	const [userListOpen, setUserListOpen] = useState(false);
 
 	const userListIconPosition = isDark ? "90.135% 3.073%" : "90.135% 11.732%";
 	const darkModeIconPosition = isDark ? "52.036% 81.18%" : "23.318% 81.006%";
@@ -37,14 +35,16 @@ export default function SideBar({
 			className={twMerge(
 				"sm:hidden fixed flex flex-col gap-8 right-0 top-0 bottom-0 w-68 bg-white z-100 rounded-l-[10px]",
 				"p-6 dark:bg-[#333333] transform transition-transform duration-300 ease-in-out",
-				userListOpen
-					? "border-0"
-					: "border-l border-[#F3F3F3] dark:border-transparent",
 				sidebarOpen ? "translate-x-0" : "translate-x-full"
 			)}
 		>
 			<Icon onClick={toggleHandler} position={closeIconPosition} size="14px" />
-			<div className="flex items-center gap-4 pb-8 border-b border-[#EAEAEA] dark:border-[#4a4b4d]">
+			<div
+				onClick={() =>
+					userInfo ? navigate(`/profile/${userData?._id}`) : navigate("/login")
+				}
+				className="flex items-center gap-4 pb-8 border-b border-[#EAEAEA] dark:border-[#4a4b4d]"
+			>
 				<img
 					src={userData?.image ? userData.image : logoCircle}
 					alt="프로필 이미지"
@@ -59,14 +59,9 @@ export default function SideBar({
 						<span className="font-medium">로그인 해주세요</span>
 					)}
 					<div
-						onClick={() =>
-							userInfo
-								? navigate(`/profile/${userData?._id}`)
-								: navigate("/login")
-						}
 						className={twMerge(
 							"flex gap-2 bg-[#06B796] px-2 py-[2px] rounded-[12px] text-white font-medium",
-							"cursor-pointer"
+							"select-none"
 						)}
 					>
 						{userData ? <span>MY</span> : <span>LOGIN</span>}
@@ -76,21 +71,13 @@ export default function SideBar({
 			</div>
 
 			<button
-				onClick={() => setUserListOpen(true)}
+				onClick={() => toggleUserList()}
 				className="flex items-center gap-4 cursor-pointer"
 			>
 				<Icon size="24px" position={userListIconPosition} />
 				<span>사용자 리스트</span>
 			</button>
-			{userListOpen && (
-				<>
-					<div className="fixed inset-0 bg-black opacity-30 z-50" />
-					<UserListModal
-						className="top-[calc(50%-290px)] left-[calc(50%-130px)]"
-						onClose={() => setUserListOpen(false)}
-					/>
-				</>
-			)}
+
 			<button
 				onClick={setDarkMode}
 				className="flex items-center gap-4 cursor-pointer"
